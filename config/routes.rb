535dd8062c93285c997ef2944f625672508f849e
anon <anon@anons-MacBook-Pro.local>
2022-10-30 23:55:42 -0700

    modifying routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,6 @@ Rails.application.routes.draw do
   # session
   get '/login', to: 'users#login_form', as: 'login'
   post '/login', to: 'users#login'
-  post '/logout', to: 'users#logout', as: 'logout'
+  get '/logout', to: 'users#logout', as: 'logout'
   get '/users/current', to: 'users#current', as: 'current_user'
 end
